0e1bc7d161bdaa295c8548003cf73c329560202f
saratcb <saratcbrandao@gmail.com>
2023-02-15 10:34:31 +0000

    generate code

--- src/academy/mindswap/game/Game.java
+++ src/academy/mindswap/game/Game.java
@@ -11,64 +11,109 @@ public class Game {
 
     private ServerSocket serverSocket;
     private ExecutorService service;
-    private final List<ClientConnectionHandler> clients;
+    private final List<ClientConnectionHandler> playersList;
+    private BufferedWriter outputName;
 
 
     public Game() {
-        clients = new CopyOnWriteArrayList<>();
+        playersList = new CopyOnWriteArrayList<>();
     }
 
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         service = Executors.newFixedThreadPool(2);
-        int numberOfConnections = 0;
         System.out.printf(Messages.SERVER_STARTED, port);
 
         while (true) {
-            acceptConnection(numberOfConnections);
-            ++numberOfConnections;
+            acceptConnection();
         }
     }
 
-    public void acceptConnection(int numberOfConnections) throws IOException {
+    public void acceptConnection() throws IOException {
         Socket clientSocket = serverSocket.accept();
+        outputName = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        outputName.write("Please insert your name!");
+        outputName.newLine();
+        outputName.flush();
+        BufferedReader inputName = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String username = inputName.readLine();
         ClientConnectionHandler clientConnectionHandler =
-                new ClientConnectionHandler(clientSocket,
-                        Messages.DEFAULT_NAME + numberOfConnections);
+                new ClientConnectionHandler(clientSocket,username);
         service.submit(clientConnectionHandler);
     }
 
     private void addClient(ClientConnectionHandler clientConnectionHandler) {
 
-        clients.add(clientConnectionHandler);
+        playersList.add(clientConnectionHandler);
         clientConnectionHandler.send(Messages.WELCOME.formatted(clientConnectionHandler.getName()));
-        clientConnectionHandler.send(Messages.COMMANDS_LIST);
-        broadcast(clientConnectionHandler.getName(), Messages.CLIENT_ENTERED_CHAT);
+       // clientConnectionHandler.send(Messages.COMMANDS_LIST);
+       // broadcast(clientConnectionHandler.getName(), Messages.CLIENT_ENTERED_CHAT);
     }
 
     public void broadcast(String name, String message) {
-        clients.stream()
+        playersList.stream()
                 .filter(handler -> !handler.getName().equals(name))
                 .forEach(handler -> handler.send(name + ": " + message));
     }
 
 
-    public String listClients() {
-        StringBuffer buffer = new StringBuffer();
-        clients.forEach(client -> buffer.append(client.getName()).append("\n"));
-        return buffer.toString();
-    }
+  //  public String listClients() {
+  //      StringBuffer buffer = new StringBuffer();
+  //      playersList.forEach(client -> buffer.append(client.getName()).append("\n"));
+  //      return buffer.toString();
+  //  }
 
     public void removeClient(ClientConnectionHandler clientConnectionHandler) {
-        clients.remove(clientConnectionHandler);
+        playersList.remove(clientConnectionHandler);
 
     }
 
     public Optional<ClientConnectionHandler> getClientByName(String name) {
-        return clients.stream()
+        return playersList.stream()
                 .filter(clientConnectionHandler -> clientConnectionHandler.getName().equalsIgnoreCase(name))
                 .findFirst();
     }
+
+    private List<Integer> generateCode() {
+        Random random = new Random();
+        List<Integer> secretCode = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            int digit = random.nextInt(5);
+            secretCode.add(digit);
+        }
+        return secretCode;
+    }
+
+    private boolean compareLists(List<Integer> list1, List<Integer> list2) {
+        if (list1.size() != list2.size()) {
+            System.out.println("We are playing with a four digit code");
+            return false;// trocar para validação de tudo
+        }
+
+        for (int i = 0; i < list1.size(); i++) {
+            if (!list1.get(i).equals(list2.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int [] compareCodes (List<Integer> code1,List<Integer>code2){
+        int numCorrectDigits = 0;
+        int numCorrectPositions; //equals com streams e fazer primeiro
+
+        for (int i = 0; i < code1.size(); i++) {
+            int digit = code1.get(i);
+            if(code2.contains(digit)){
+                numCorrectDigits ++;
+            }
+
+        }int [] result ={numCorrectDigits,numCorrectPositions};
+        return result;
+    }
+
 
     public class ClientConnectionHandler implements Runnable {
 
