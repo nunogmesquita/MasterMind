89e37ee54c303dc3e191791e58a417f5d4f68870
Nuno Gil Mesquita <nunomesquita2@gmail.com>
2023-02-20 09:57:11 +0000

    Code

--- src/academy/mindswap/game/Code.java
+++ src/academy/mindswap/game/Code.java
@@ -1,37 +1,49 @@
 package academy.mindswap.game;
 
+import academy.mindswap.game.messages.Messages;
+
 import java.util.*;
 
 public class Code {
 
+    private final List<Server.ConnectedPlayer> p2pList;
+
+    public HashMap<Integer, ArrayList<String>> playerCodes;
+
+    private static List<String> compareResults;
+
+    public Code() {
+        this.p2pList = new ArrayList<>();
+        this.playerCodes = new HashMap<>();
+    }
+
     static ArrayList<String> generateCode() {
         ArrayList<String> code = new ArrayList<>();
-        ArrayList<String> possibleChoices = new ArrayList<>(Arrays.asList("G","B","Y","O","P"));
-        for (int i = 0; i < 4 ; i++) {
+        ArrayList<String> possibleChoices = new ArrayList<>(Arrays.asList("G", "B", "Y", "O", "P"));
+        for (int i = 0; i < 4; i++) {
             code.add(possibleChoices.get(new Random().nextInt(5)));
         }
         return code;
     }
 
-    static boolean rightGuess(Game game, List<String> playerGuess) {
-        if (game.secretCode.equals(playerGuess)) {
-            for (int i = 0; i < game.secretCode.size(); i++) {
-                game.turnResult.add("+");
+    static boolean rightGuess(List<String> playerGuess, List<String> secretCode) {
+        if (secretCode.equals(playerGuess)) {
+            for (int i = 0; i < secretCode.size(); i++) {
+                compareResults.add("+");
             }
-            game.rightGuess = true;
             return true;
         }
         return false;
     }
 
-    static void compareCodes(Game game, List<String> playerGuess, List<String> secretCode) {
-        game.turnResult = new ArrayList<>();
-        if (!rightGuess(game, playerGuess)) {
+    static List<String> compareCodes(List<String> playerGuess, List<String> secretCode) {
+        compareResults = new ArrayList<>();
+        if (!rightGuess(playerGuess, secretCode)) {
             List<String> playerGuessCopy = new ArrayList<>(playerGuess);
             List<String> secretCodeCopy = new ArrayList<>(secretCode);
             for (int i = 0; i < playerGuess.size(); i++) {
                 if (playerGuessCopy.get(i).equals(secretCodeCopy.get(i)) && playerGuessCopy.get(i) != null) {
-                    game.turnResult.add("+");
+                    compareResults.add("+");
                     playerGuessCopy.set(i, null);
                     secretCodeCopy.set(i, null);
                 }
@@ -40,14 +52,18 @@ public class Code {
             for (int i = 0; i < playerGuessCopy.stream()
                     .filter(secretCodeCopy::contains)
                     .filter(Objects::nonNull).count(); i++) {
-                game.turnResult.add("-");
+                compareResults.add("-");
             }
-            while (game.turnResult.size() != playerGuess.size()) {
-                game.turnResult.add(" ");
+            while (compareResults.size() != playerGuess.size()) {
+                compareResults.add(" ");
             }
             playerGuessCopy.clear();
             secretCodeCopy.clear();
         }
+        return compareResults;
+    }
 
+    public static void showCode(Server.ConnectedPlayer player) {
+        player.send(Messages.SHOW_CODE.formatted(player.game.secretCode));
     }
 }
